81a7613828646385756052b84f01d2c527b4df67
Lion-Ga92 <Luis@tamalesgarcia.com>
2020-06-04 23:37:25 -0700

    Added new instation of Spell class

--- Classes/main.py
+++ Classes/main.py
@@ -1,11 +1,24 @@
 from Classes.game import Person, bcolors
+from Classes.Magic import Spell
+# Create black magic
+fire = Spell("Fire", 10, 100, "Black")
+thunder = Spell("Thunder", 10, 100, "Black")
+blizzard = Spell("Blizzard", 10, 100, "Black")
+meteor = Spell("Meteor", 20, 200, "Black")
+quake = Spell("Quake", 14, 140, "Black")
+
+# Create white magic
+cure = Spell("Cure", 12, 120, "white")
+cura = Spell("Cura", 18, 200, "white")
+
 
 magic = [{"name": "Fire", "cost": 10, "dmg": 100},
          {"name": "Thunder", "cost": 10, "dmg": 124},
          {"name": "Blizzard", "cost": 10, "dmg": 100}]
 
-player = Person(460, 65, 60, 34, magic)
-enemy = Person(1200, 65, 45, 25, magic)
+#instatiate people
+player = Person(460, 65, 60, 34, [fire, thunder, blizzard, meteor, quake, cure, cura])
+enemy = Person(1200, 65, 45, 25, [])
 
 running = True
 
@@ -25,17 +38,17 @@ while running:
     elif index == 1:
         player.choose_magic()
         magic_choice = int(input("Choose magic: ")) - 1
-        magic_dmg = player.generate_spell_damage(magic_choice)
-        spell = player.get_spell_name(magic_choice)
-        cost = player.get_spell_mp_cost(magic_choice)
+
+        spell = player.magic[magic_choice]
+        magic_dmg = spell.generate_damage()
 
         current_mp = player.get_mp()
-        if cost > current_mp:
+        if spell.cost > current_mp:
             print(bcolors.FAIL + "\n NOT ENOUGH MP\n" + bcolors.END)
 
-        player.reduce_mp(cost)
+        player.reduce_mp(spell.cost)
         enemy.take_damage(magic_dmg)
-        print(bcolors.OKBLUE + "\n" + spell + " deals", str(magic_dmg), "points of damage" + bcolors.END)
+        print(bcolors.OKBLUE + "\n" + spell.name + " deals", str(magic_dmg), "points of damage" + bcolors.END)
 
     enemy_choice = 1
 
@@ -55,4 +68,3 @@ while running:
     elif player.get_hp() == 0:
         print(bcolors.FAIL + "Your enemy has defeated you!" + bcolors.END)
         running = False
-print("Testing testing")
